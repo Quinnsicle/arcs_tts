810525d7d35f6d4618d048739969363a1e7d8c6d
scyth02 <scyth02@gmail.com>
2023-12-21 14:34:28 -0500

    Code cleanup and removal of container event functions.

--- src/Counters.lua
+++ src/Counters.lua
@@ -2,158 +2,158 @@
 
 local ObjectCounters = {}
 
-local Has_Counter = {}
+local has_counter = {}
+local the_counters = {
+  {
+    GUID = Global.getVar("player_pieces_GUIDs")["White"]["ships"],
+    position = {0.5, 0.06, 0.03},
+    shadow = {0.53, 0.06, 0.05}, -- drop shadow bottom right
+    -- shadow = {0.48, 0.06, 0.04}, --drop shadow bottom left
+    width = 1,
+    height = 1,
+    scale = {1, 1, 1},
+    font_size = 365,
+    font_color = {1, 1, 1}
+  },
+  {
+    GUID = Global.getVar("player_pieces_GUIDs")["White"]["agents"],
+    position = {0.5, 0.06, 0.03},
+    shadow = {0.53, 0.06, 0.05}, -- drop shadow bottom right
+    -- shadow = {0.48, 0.06, 0.04}, --drop shadow bottom left
+    width = 1,
+    height = 1,
+    scale = {1, 1, 1},
+    font_size = 365,
+    font_color = {1, 1, 1}
+  },
+  {
+    GUID = Global.getVar("player_pieces_GUIDs")["White"]["starports"],
+    position = {0.5, 0.06, 0.03},
+    shadow = {0.53, 0.06, 0.05}, -- drop shadow bottom right
+    -- shadow = {0.48, 0.06, 0.04}, --drop shadow bottom left
+    width = 1,
+    height = 1,
+    scale = {1, 1, 1},
+    font_size = 365,
+    font_color = {1, 1, 1}
+  },
+  {
+    GUID = Global.getVar("player_pieces_GUIDs")["Yellow"]["ships"],
+    position = {0.5, 0.06, 0.03},
+    shadow = {0.53, 0.06, 0.05}, -- drop shadow bottom right
+    -- shadow = {0.48, 0.06, 0.04}, --drop shadow bottom left
+    width = 1,
+    height = 1,
+    scale = {1, 1, 1},
+    font_size = 365,
+    font_color = {1, 1, 1}
+  },
+  {
+    GUID = Global.getVar("player_pieces_GUIDs")["Yellow"]["agents"],
+    position = {0.5, 0.06, 0.03},
+    shadow = {0.53, 0.06, 0.05}, -- drop shadow bottom right
+    -- shadow = {0.48, 0.06, 0.04}, --drop shadow bottom left
+    width = 1,
+    height = 1,
+    scale = {1, 1, 1},
+    font_size = 365,
+    font_color = {1, 1, 1}
+  },
+  {
+    GUID = Global.getVar("player_pieces_GUIDs")["Yellow"]["starports"],
+    position = {0.5, 0.06, 0.03},
+    shadow = {0.53, 0.06, 0.05}, -- drop shadow bottom right
+    -- shadow = {0.48, 0.06, 0.04}, --drop shadow bottom left
+    width = 1,
+    height = 1,
+    scale = {1, 1, 1},
+    font_size = 365,
+    font_color = {1, 1, 1}
+  },
+  {
+    GUID = Global.getVar("player_pieces_GUIDs")["Red"]["ships"],
+    position = {0.5, 0.06, 0.03},
+    shadow = {0.53, 0.06, 0.05}, -- drop shadow bottom right
+    -- shadow = {0.48, 0.06, 0.04}, --drop shadow bottom left
+    width = 1,
+    height = 1,
+    scale = {1, 1, 1},
+    font_size = 365,
+    font_color = {1, 1, 1}
+  },
+  {
+    GUID = Global.getVar("player_pieces_GUIDs")["Red"]["agents"],
+    position = {0.5, 0.06, 0.03},
+    shadow = {0.53, 0.06, 0.05}, -- drop shadow bottom right
+    -- shadow = {0.48, 0.06, 0.04}, --drop shadow bottom left
+    width = 1,
+    height = 1,
+    scale = {1, 1, 1},
+    font_size = 365,
+    font_color = {1, 1, 1}
+  },
+  {
+    GUID = Global.getVar("player_pieces_GUIDs")["Red"]["starports"],
+    position = {0.5, 0.06, 0.03},
+    shadow = {0.53, 0.06, 0.05}, -- drop shadow bottom right
+    -- shadow = {0.48, 0.06, 0.04}, --drop shadow bottom left
+    width = 1,
+    height = 1,
+    scale = {1, 1, 1},
+    font_size = 365,
+    font_color = {1, 1, 1}
+  },
+  {
+    GUID = Global.getVar("player_pieces_GUIDs")["Teal"]["ships"],
+    position = {0.5, 0.06, 0.03},
+    shadow = {0.53, 0.06, 0.05}, -- drop shadow bottom right
+    -- shadow = {0.48, 0.06, 0.04}, --drop shadow bottom left
+    width = 1,
+    height = 1,
+    scale = {1, 1, 1},
+    font_size = 365,
+    font_color = {1, 1, 1}
+  },
+  {
+    GUID = Global.getVar("player_pieces_GUIDs")["Teal"]["agents"],
+    position = {0.5, 0.06, 0.03},
+    shadow = {0.53, 0.06, 0.05}, -- drop shadow bottom right
+    -- shadow = {0.48, 0.06, 0.04}, --drop shadow bottom left
+    width = 1,
+    height = 1,
+    scale = {1, 1, 1},
+    font_size = 365,
+    font_color = {1, 1, 1}
+  },
+  {
+    GUID = Global.getVar("player_pieces_GUIDs")["Teal"]["starports"],
+    position = {0.5, 0.06, 0.03},
+    shadow = {0.53, 0.06, 0.05}, -- drop shadow bottom right
+    -- shadow = {0.48, 0.06, 0.04}, --drop shadow bottom left
+    width = 1,
+    height = 1,
+    scale = {1, 1, 1},
+    font_size = 365,
+    font_color = {1, 1, 1}
+  },
+  {
+    GUID = Global.getVar("imperial_ships_GUID"),
+    position = {0.5, 0.06, 0.03},
+    shadow = {0.53, 0.06, 0.05}, -- drop shadow bottom right
+    -- shadow = {0.48, 0.06, 0.04}, --drop shadow bottom left
+    width = 1,
+    height = 1,
+    scale = {1, 1, 1},
+    font_size = 365,
+    font_color = {0.8, 0.58, 0.27}
+  } --]]
+}
 
 function ObjectCounters.setup()
-  local Counters = {
-    {
-      GUID = Global.getVar("player_pieces_GUIDs")["White"]["ships"],
-      position = {0.5, 0.06, 0.03},
-      shadow = {0.53, 0.06, 0.05}, -- drop shadow bottom right
-      -- shadow = {0.48, 0.06, 0.04}, --drop shadow bottom left
-      width = 1,
-      height = 1,
-      scale = {1, 1, 1},
-      font_size = 365,
-      font_color = {1, 1, 1}
-    },
-    {
-      GUID = Global.getVar("player_pieces_GUIDs")["White"]["agents"],
-      position = {0.5, 0.06, 0.03},
-      shadow = {0.53, 0.06, 0.05}, -- drop shadow bottom right
-      -- shadow = {0.48, 0.06, 0.04}, --drop shadow bottom left
-      width = 1,
-      height = 1,
-      scale = {1, 1, 1},
-      font_size = 365,
-      font_color = {1, 1, 1}
-    },
-    {
-      GUID = Global.getVar("player_pieces_GUIDs")["White"]["starports"],
-      position = {0.5, 0.06, 0.03},
-      shadow = {0.53, 0.06, 0.05}, -- drop shadow bottom right
-      -- shadow = {0.48, 0.06, 0.04}, --drop shadow bottom left
-      width = 1,
-      height = 1,
-      scale = {1, 1, 1},
-      font_size = 365,
-      font_color = {1, 1, 1}
-    },
-    {
-      GUID = Global.getVar("player_pieces_GUIDs")["Yellow"]["ships"],
-      position = {0.5, 0.06, 0.03},
-      shadow = {0.53, 0.06, 0.05}, -- drop shadow bottom right
-      -- shadow = {0.48, 0.06, 0.04}, --drop shadow bottom left
-      width = 1,
-      height = 1,
-      scale = {1, 1, 1},
-      font_size = 365,
-      font_color = {1, 1, 1}
-    },
-    {
-      GUID = Global.getVar("player_pieces_GUIDs")["Yellow"]["agents"],
-      position = {0.5, 0.06, 0.03},
-      shadow = {0.53, 0.06, 0.05}, -- drop shadow bottom right
-      -- shadow = {0.48, 0.06, 0.04}, --drop shadow bottom left
-      width = 1,
-      height = 1,
-      scale = {1, 1, 1},
-      font_size = 365,
-      font_color = {1, 1, 1}
-    },
-    {
-      GUID = Global.getVar("player_pieces_GUIDs")["Yellow"]["starports"],
-      position = {0.5, 0.06, 0.03},
-      shadow = {0.53, 0.06, 0.05}, -- drop shadow bottom right
-      -- shadow = {0.48, 0.06, 0.04}, --drop shadow bottom left
-      width = 1,
-      height = 1,
-      scale = {1, 1, 1},
-      font_size = 365,
-      font_color = {1, 1, 1}
-    },
-    {
-      GUID = Global.getVar("player_pieces_GUIDs")["Red"]["ships"],
-      position = {0.5, 0.06, 0.03},
-      shadow = {0.53, 0.06, 0.05}, -- drop shadow bottom right
-      -- shadow = {0.48, 0.06, 0.04}, --drop shadow bottom left
-      width = 1,
-      height = 1,
-      scale = {1, 1, 1},
-      font_size = 365,
-      font_color = {1, 1, 1}
-    },
-    {
-      GUID = Global.getVar("player_pieces_GUIDs")["Red"]["agents"],
-      position = {0.5, 0.06, 0.03},
-      shadow = {0.53, 0.06, 0.05}, -- drop shadow bottom right
-      -- shadow = {0.48, 0.06, 0.04}, --drop shadow bottom left
-      width = 1,
-      height = 1,
-      scale = {1, 1, 1},
-      font_size = 365,
-      font_color = {1, 1, 1}
-    },
-    {
-      GUID = Global.getVar("player_pieces_GUIDs")["Red"]["starports"],
-      position = {0.5, 0.06, 0.03},
-      shadow = {0.53, 0.06, 0.05}, -- drop shadow bottom right
-      -- shadow = {0.48, 0.06, 0.04}, --drop shadow bottom left
-      width = 1,
-      height = 1,
-      scale = {1, 1, 1},
-      font_size = 365,
-      font_color = {1, 1, 1}
-    },
-    {
-      GUID = Global.getVar("player_pieces_GUIDs")["Teal"]["ships"],
-      position = {0.5, 0.06, 0.03},
-      shadow = {0.53, 0.06, 0.05}, -- drop shadow bottom right
-      -- shadow = {0.48, 0.06, 0.04}, --drop shadow bottom left
-      width = 1,
-      height = 1,
-      scale = {1, 1, 1},
-      font_size = 365,
-      font_color = {1, 1, 1}
-    },
-    {
-      GUID = Global.getVar("player_pieces_GUIDs")["Teal"]["agents"],
-      position = {0.5, 0.06, 0.03},
-      shadow = {0.53, 0.06, 0.05}, -- drop shadow bottom right
-      -- shadow = {0.48, 0.06, 0.04}, --drop shadow bottom left
-      width = 1,
-      height = 1,
-      scale = {1, 1, 1},
-      font_size = 365,
-      font_color = {1, 1, 1}
-    },
-    {
-      GUID = Global.getVar("player_pieces_GUIDs")["Teal"]["starports"],
-      position = {0.5, 0.06, 0.03},
-      shadow = {0.53, 0.06, 0.05}, -- drop shadow bottom right
-      -- shadow = {0.48, 0.06, 0.04}, --drop shadow bottom left
-      width = 1,
-      height = 1,
-      scale = {1, 1, 1},
-      font_size = 365,
-      font_color = {1, 1, 1}
-    },
-    {
-      GUID = Global.getVar("imperial_ships_GUID"),
-      position = {0.5, 0.06, 0.03},
-      shadow = {0.53, 0.06, 0.05}, -- drop shadow bottom right
-      -- shadow = {0.48, 0.06, 0.04}, --drop shadow bottom left
-      width = 1,
-      height = 1,
-      scale = {1, 1, 1},
-      font_size = 365,
-      font_color = {0.8, 0.58, 0.27}
-    }
-  }
 
   -- Begin implementing buttons
-  for _, counter in ipairs(Counters) do
-    Has_Counter[counter.GUID] = true
+  for _, counter in pairs(the_counters) do
+    has_counter[counter.GUID] = true
     local container = getObjectFromGUID(counter.GUID)
     container.createButton({
       function_owner  = self,
@@ -184,12 +184,8 @@ function ObjectCounters.setup()
 
 end
 
-function onObjectEnterContainer(container, object) ObjectCounters.Update(container) end
-function onObjectLeaveContainer(container, object) ObjectCounters.Update(container) end
-function onObjectEnterZone(zone, object) ObjectCounters.Update(zone) end
-function onObjectLeaveZone(zone, object) ObjectCounters.Update(zone) end
-function ObjectCounters.Update(container)
-  if Has_Counter[container.getGUID()] then
+function ObjectCounters.update(container)
+  if has_counter[container.getGUID()] then
     container.editButton({
       index = 0,
       label = "" .. #container.getObjects()
