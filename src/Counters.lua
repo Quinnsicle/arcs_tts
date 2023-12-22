b81d3b18290a0a06a7dd91ac3178ce2551dbb0b8
scyth02 <scyth02@gmail.com>
2023-12-21 20:05:12 -0500

    Implemented add function so new counters can be created outside module.

--- src/Counters.lua
+++ src/Counters.lua
@@ -5,7 +5,7 @@ local ObjectCounters = {}
 local has_counter = {}
 local the_counters = {
   {
-    GUID = Global.getVar("player_pieces_GUIDs")["White"]["ships"],
+    container_GUID = Global.getVar("player_pieces_GUIDs")["White"]["ships"],
     position = {0.5, 0.06, 0.03},
     shadow = {0.53, 0.06, 0.05}, -- drop shadow bottom right
     -- shadow = {0.48, 0.06, 0.04}, --drop shadow bottom left
@@ -16,7 +16,7 @@ local the_counters = {
     font_color = {1, 1, 1}
   },
   {
-    GUID = Global.getVar("player_pieces_GUIDs")["White"]["agents"],
+    container_GUID = Global.getVar("player_pieces_GUIDs")["White"]["agents"],
     position = {0.5, 0.06, 0.03},
     shadow = {0.53, 0.06, 0.05}, -- drop shadow bottom right
     -- shadow = {0.48, 0.06, 0.04}, --drop shadow bottom left
@@ -27,7 +27,7 @@ local the_counters = {
     font_color = {1, 1, 1}
   },
   {
-    GUID = Global.getVar("player_pieces_GUIDs")["White"]["starports"],
+    container_GUID = Global.getVar("player_pieces_GUIDs")["White"]["starports"],
     position = {0.5, 0.06, 0.03},
     shadow = {0.53, 0.06, 0.05}, -- drop shadow bottom right
     -- shadow = {0.48, 0.06, 0.04}, --drop shadow bottom left
@@ -38,7 +38,7 @@ local the_counters = {
     font_color = {1, 1, 1}
   },
   {
-    GUID = Global.getVar("player_pieces_GUIDs")["Yellow"]["ships"],
+    container_GUID = Global.getVar("player_pieces_GUIDs")["Yellow"]["ships"],
     position = {0.5, 0.06, 0.03},
     shadow = {0.53, 0.06, 0.05}, -- drop shadow bottom right
     -- shadow = {0.48, 0.06, 0.04}, --drop shadow bottom left
@@ -49,7 +49,7 @@ local the_counters = {
     font_color = {1, 1, 1}
   },
   {
-    GUID = Global.getVar("player_pieces_GUIDs")["Yellow"]["agents"],
+    container_GUID = Global.getVar("player_pieces_GUIDs")["Yellow"]["agents"],
     position = {0.5, 0.06, 0.03},
     shadow = {0.53, 0.06, 0.05}, -- drop shadow bottom right
     -- shadow = {0.48, 0.06, 0.04}, --drop shadow bottom left
@@ -60,7 +60,7 @@ local the_counters = {
     font_color = {1, 1, 1}
   },
   {
-    GUID = Global.getVar("player_pieces_GUIDs")["Yellow"]["starports"],
+    container_GUID = Global.getVar("player_pieces_GUIDs")["Yellow"]["starports"],
     position = {0.5, 0.06, 0.03},
     shadow = {0.53, 0.06, 0.05}, -- drop shadow bottom right
     -- shadow = {0.48, 0.06, 0.04}, --drop shadow bottom left
@@ -71,7 +71,7 @@ local the_counters = {
     font_color = {1, 1, 1}
   },
   {
-    GUID = Global.getVar("player_pieces_GUIDs")["Red"]["ships"],
+    container_GUID = Global.getVar("player_pieces_GUIDs")["Red"]["ships"],
     position = {0.5, 0.06, 0.03},
     shadow = {0.53, 0.06, 0.05}, -- drop shadow bottom right
     -- shadow = {0.48, 0.06, 0.04}, --drop shadow bottom left
@@ -82,7 +82,7 @@ local the_counters = {
     font_color = {1, 1, 1}
   },
   {
-    GUID = Global.getVar("player_pieces_GUIDs")["Red"]["agents"],
+    container_GUID = Global.getVar("player_pieces_GUIDs")["Red"]["agents"],
     position = {0.5, 0.06, 0.03},
     shadow = {0.53, 0.06, 0.05}, -- drop shadow bottom right
     -- shadow = {0.48, 0.06, 0.04}, --drop shadow bottom left
@@ -93,7 +93,7 @@ local the_counters = {
     font_color = {1, 1, 1}
   },
   {
-    GUID = Global.getVar("player_pieces_GUIDs")["Red"]["starports"],
+    container_GUID = Global.getVar("player_pieces_GUIDs")["Red"]["starports"],
     position = {0.5, 0.06, 0.03},
     shadow = {0.53, 0.06, 0.05}, -- drop shadow bottom right
     -- shadow = {0.48, 0.06, 0.04}, --drop shadow bottom left
@@ -104,7 +104,7 @@ local the_counters = {
     font_color = {1, 1, 1}
   },
   {
-    GUID = Global.getVar("player_pieces_GUIDs")["Teal"]["ships"],
+    container_GUID = Global.getVar("player_pieces_GUIDs")["Teal"]["ships"],
     position = {0.5, 0.06, 0.03},
     shadow = {0.53, 0.06, 0.05}, -- drop shadow bottom right
     -- shadow = {0.48, 0.06, 0.04}, --drop shadow bottom left
@@ -115,7 +115,7 @@ local the_counters = {
     font_color = {1, 1, 1}
   },
   {
-    GUID = Global.getVar("player_pieces_GUIDs")["Teal"]["agents"],
+    container_GUID = Global.getVar("player_pieces_GUIDs")["Teal"]["agents"],
     position = {0.5, 0.06, 0.03},
     shadow = {0.53, 0.06, 0.05}, -- drop shadow bottom right
     -- shadow = {0.48, 0.06, 0.04}, --drop shadow bottom left
@@ -126,7 +126,7 @@ local the_counters = {
     font_color = {1, 1, 1}
   },
   {
-    GUID = Global.getVar("player_pieces_GUIDs")["Teal"]["starports"],
+    container_GUID = Global.getVar("player_pieces_GUIDs")["Teal"]["starports"],
     position = {0.5, 0.06, 0.03},
     shadow = {0.53, 0.06, 0.05}, -- drop shadow bottom right
     -- shadow = {0.48, 0.06, 0.04}, --drop shadow bottom left
@@ -137,7 +137,7 @@ local the_counters = {
     font_color = {1, 1, 1}
   },
   {
-    GUID = Global.getVar("imperial_ships_GUID"),
+    container_GUID = Global.getVar("imperial_ships_GUID"),
     position = {0.5, 0.06, 0.03},
     shadow = {0.53, 0.06, 0.05}, -- drop shadow bottom right
     -- shadow = {0.48, 0.06, 0.04}, --drop shadow bottom left
@@ -150,38 +150,39 @@ local the_counters = {
 }
 
 function ObjectCounters.setup()
-
-  -- Begin implementing buttons
+  log("test")
   for _, counter in pairs(the_counters) do
-    has_counter[counter.GUID] = true
-    local container = getObjectFromGUID(counter.GUID)
-    container.createButton({
-      function_owner  = self,
-      click_function  = "doNothing",
-      label           = ""..#container.getObjects(),
-      position        = counter.shadow,
-      rotation        = counter.rotation and counter.rotation or {0,0,0},
-      width           = counter.width,
-      height          = counter.height,
-      scale           = counter.scale,
-      font_size       = counter.font_size,
-      font_color      = {0, 0, 0}
-    })
-    container.createButton({
-      function_owner  = self,
-      click_function  = "doNothing",
-      label           = ""..#container.getObjects(),
-      position        = counter.position,
-      rotation        = counter.rotation and counter.rotation or {0,0,0},
-      width           = counter.width,
-      height          = counter.height,
-      scale           = counter.scale,
-      font_size       = counter.font_size,
-      font_color      = counter.font_color
-    })
-    --log("Attached counter to: "..getObjectFromGUID(counter.GUID).getName())
+    ObjectCounters.add(getObjectFromGUID(counter.container_GUID), counter)
   end
+end
 
+function ObjectCounters.add(container, button)
+  has_counter[container.getGUID()] = true
+  container.createButton({
+    function_owner  = self,
+    click_function  = "doNothing",
+    label           = ""..#container.getObjects(),
+    position        = button.shadow,
+    rotation        = button.rotation and button.rotation or {0,0,0},
+    width           = button.width,
+    height          = button.height,
+    scale           = button.scale,
+    font_size       = button.font_size,
+    font_color      = {0, 0, 0}
+  })
+  container.createButton({
+    function_owner  = self,
+    click_function  = "doNothing",
+    label           = ""..#container.getObjects(),
+    position        = button.position,
+    rotation        = button.rotation and button.rotation or {0,0,0},
+    width           = button.width,
+    height          = button.height,
+    scale           = button.scale,
+    font_size       = button.font_size,
+    font_color      = button.font_color
+  })
+  --log("Attached counter to: "..container.getName())
 end
 
 function ObjectCounters.update(container)
