143856395d9e0c8a872ec363a1033573d60df11a
scyth02 <scyth02@gmail.com>
2024-01-05 19:03:51 -0500

    Updated initiative marker to move relative to player boards since zone is not in save.

--- tts_src/Global.-1.lua
+++ tts_src/Global.-1.lua
@@ -1,14 +1,6 @@
 available_colors = {"White", "Yellow", "Red", "Teal"}
 
-debug_player_count = 2
-
-FUDiscard_zone_GUID = "c56672"
-FUDiscard_marker_GUID = "000207"
-lead_card_zone_GUID = "629039"
-
 setup_table_GUID = "95f3f9"
-reach_board_GUID = "bb7d21"
-removed_from_game_GUID = "9795fb"
 -- leaders and lore
 more_to_explore_expansion_button_GUID = "9ff1fd"
 more_to_explore_fate_GUID = "bbd744"
@@ -18,17 +10,11 @@ with_more_to_explore = false
 include_fates_GUID = "be0e27"
 with_leaders = false
 
-action_deck_GUID = "cf0c25"
-action_deck_4P_GUID = "22a45c"
+action_deck_GUID = "855643"
+action_deck_4P_GUID = "9128ca"
 action_card_zone_GUID = "e6eca7"
-
+with_split_discard = false
 ambition_marker_zone_GUID = "3984e4"
-ambition_declared_marker_GUID = "65f9a2"
-ambition_marker_GUIDs = {
-    "c9e0ee",
-    "a9b02a",
-    "b0b4d0"
-}
 court_deck_zone_GUID = "7a33ff"
 
 fate_GUID = "f96461"
@@ -65,49 +51,9 @@ oop_large_gate2_GUID = "9bd528"
 oop_planet_GUIDs = {"6ecb02", "795637", "ae5114", "d5fe1a", "0d3d0c",
                     "4d14cb"}
 
--- new out of play pieces
-oop_components = {
-    {
-        Sector = { pos = {-0.17,0.97,-1.04}, rot = {0,180,-0.01}, scale = {2.48,1,2.48},
-        img = "http://cloud-3.steamusercontent.com/ugc/2313225941445769502/1D85B9468BB538D788FCF7576A05606918CD0DD4/" },
-        Gate = { pos = {-0.04,0.97,-0.63}, rot = {0,189.24,-0.01}, scale = {0.71,1,0.71},
-        img = "http://cloud-3.steamusercontent.com/ugc/2313225941445769214/A4AD66554742C2FFA93612948C38641B813947FB/" }
-    },
-    {
-        Sector = { pos = {-0.51,0.97,-0.66}, rot = {0,180,-0.01}, scale = {2.48,1,2.48},
-        img = "http://cloud-3.steamusercontent.com/ugc/2313225941445769605/A40A0C79B27F1F1C45E0570E46BA8A7B253F356E/" },
-        Gate = { pos = {-0.23,0.97,-0.21}, rot = {0,252.52,0}, scale = {0.44,1,0.44},
-        img = "http://cloud-3.steamusercontent.com/ugc/2313225941445769422/DFF68E0F82851F1AAE746B676B40470DDF3B2FBC/" }
-    },
-    {
-        Sector = { pos = {-0.47,0.97,0.73}, rot = {0,179.99,-0.01}, scale = {2.36,1,2.36},
-        img = "http://cloud-3.steamusercontent.com/ugc/2313225941445769710/C408A11914F7F4DEA83686851730DDF10A8BD5D4/" },
-        Gate = { pos = {-0.2,0.97,0.28}, rot = {0,305.16,0}, scale = {0.44,1,0.44},
-        img = "http://cloud-3.steamusercontent.com/ugc/2313225941445769422/DFF68E0F82851F1AAE746B676B40470DDF3B2FBC/" }
-    },
-    {
-        Sector = { pos = {0.17,0.97,0.91}, rot = {0,180,-0.01}, scale = {2.54,1,2.54},
-        img = "http://cloud-3.steamusercontent.com/ugc/2313225941445769816/0AA42154550040133E7D6740F85CD487D5F6967B/" },
-        Gate = { pos = {0.05,0.97,0.52}, rot = {-0.01,12.02,0}, scale = {0.71,1,0.71},
-        img = "http://cloud-3.steamusercontent.com/ugc/2313225941445769214/A4AD66554742C2FFA93612948C38641B813947FB/" }
-    },
-    {
-        Sector = { pos = {0.5,0.97,0.55}, rot = {0,179.99,-0.01}, scale = {2.48,1,2.48},
-        img = "http://cloud-3.steamusercontent.com/ugc/2313225941445770194/8600421030523070B8E2F05CECC3281DF24989AC/" },
-        Gate = { pos = {0.24,0.97,0.1}, rot = {-0.01,72.87,-0.01}, scale = {0.44,1,0.44},
-        img = "http://cloud-3.steamusercontent.com/ugc/2313225941445769422/DFF68E0F82851F1AAE746B676B40470DDF3B2FBC/" }
-    },
-    {
-        Sector = { pos = {0.46,0.97,-0.85}, rot = {0,179.99,-0.01}, scale = {2.29,1,2.29},
-        img = "http://cloud-3.steamusercontent.com/ugc/2313225941445770362/76677A077FC1D6CD3672DCC036646ABFD2881F62/" },
-        Gate = { pos = {0.2,0.97,-0.39}, rot = {-0.01,125.02,-0.01}, scale = {0.44,1,0.44},
-        img = "http://cloud-3.steamusercontent.com/ugc/2313225941445769422/DFF68E0F82851F1AAE746B676B40470DDF3B2FBC/" }
-    }
-}
-
 initiative_GUID = "b3b3d0"
 seized_initiative_GUID = "e0f490"
-initiative_pos = {-2,0,0}
+initaitive_player_position = {-2,0,0}
 
 chapter_pawn_GUID = "9c3ac8"
 
@@ -118,6 +64,7 @@ is_player_setup = {
     Red = false,
     Teal = false
 }
+
 -- Players Pieces
 player_pieces_GUIDs = {
     ["White"] = {
@@ -126,6 +73,7 @@ player_pieces_GUIDs = {
         city = {"822a9c", "00ee1b"},
         ships = "6883e6",
         starports = "b96445",
+        agents = "c863eb",
         cities = {
             "822a9c",
             "00ee1b",
@@ -133,7 +81,6 @@ player_pieces_GUIDs = {
             "06f4a8",
             "81c3a7"
         },
-        agents = "c863eb",
         initiative_zone = "2e1cd3",
         trophies_zone = "275a50",
         captives_zone = "0c07a0",
@@ -145,6 +92,7 @@ player_pieces_GUIDs = {
         city = {"dbf4de", "799077"},
         ships = "a75924",
         starports = "b9ebd3",
+        agents = "7b3749",
         cities = {
             "dbf4de",
             "799077",
@@ -152,7 +100,6 @@ player_pieces_GUIDs = {
             "ac28fb",
             "b41592"
         },
-        agents = "7b3749",
         initiative_zone = "3fc6fd",
         trophies_zone = "7f5014",
         captives_zone = "31a56f",
@@ -164,6 +111,7 @@ player_pieces_GUIDs = {
         city = {"33577c", "cf5b95"},
         ships = "7e0fe2",
         starports = "51a8f5",
+        agents = "bbb3aa",
         cities = {
             "33577c",
             "cf5b95",
@@ -171,7 +119,6 @@ player_pieces_GUIDs = {
             "6e36ca",
             "282f37"
         },
-        agents = "bbb3aa",
         initiative_zone = "32f290",
         trophies_zone = "48b6fb",
         captives_zone = "7b011e",
@@ -183,6 +130,7 @@ player_pieces_GUIDs = {
         city = {"79b799", "f3da7f"},
         ships = "2da385",
         starports = "7e625d",
+        agents = "791097",
         cities = {
             "f3da7f",
             "5e753e",
@@ -190,10 +138,9 @@ player_pieces_GUIDs = {
             "fad0f1",
             "45c804"
         },
-        agents = "791097",
         initiative_zone = "cdc545",
-        trophies_zone = "48b6fb",
-        captives_zone = "7b011e",
+        trophies_zone = "3085c9",
+        captives_zone = "fe0b0d",
         area_zone = "ee4b6e"
     }
 }
@@ -308,6 +255,13 @@ resources_markers_GUID = {
     materials = "eb1cba"
 }
 
+merchant_tycoon_GUID = "612aa9"
+merchant_GUID = {
+    tycoon = "612aa9",
+    warlord = "07c555",
+    keeper = "975e47",
+    empath = "99b2ba"
+}
 ----------------------------------------------------
 -- campaign
 ----------------------------------------------------
@@ -316,7 +270,7 @@ control_GUID = "6e21fe"
 
 campaign_setup_GUID = "4c00fe"
 
-event_deck_GUID = "ad423d"
+event_deck_GUID = "7be749"
 chapter_track_GUID = "4d34d7"
 chapter_zone_GUID = "2d2c49"
 
@@ -339,10 +293,9 @@ blight_GUID = "3c61d2"
 A_Fates_GUID = "0ac7d1"
 
 ----------------------------------------------------
-local Counters = require("src/Counters")
 local Supplies = require("src/Supplies")
+local Counters = require("src/Counters")
 
--- Player Events --
 function assignPlayerToAvailableColor(player, color)
     local color = table.remove(available_colors, 1)
     broadcastToAll(
@@ -358,31 +311,38 @@ function onPlayerDisconnect(player)
     table.insert(available_colors, 1, player.color)
 end
 
+function onObjectEnterZone(zone,object)
+    Counters.update(zone)
+end
+
 function onObjectSpawn(object)
     Supplies.addMenuToObject(object)
 end
 
--- Container Events --
+function onObjectLeaveZone(zone,object)
+    Counters.update(zone)
+end
+
+function onObjectEnterContainer(container,object)
+    Counters.update(container)
+end
+
 function onObjectLeaveContainer(container, leave_object)
+    Counters.update(container)
     if container.type == "Deck" or container.type == "Bag" or
         container.type == "Infinite" then
-
         leave_object.setTags(container.getTags())
 
         -- set snap
         leave_object.use_snap_points = true
-
-        Counters.update(container)
-
     end
 end
 
 function tryObjectEnterContainer(container, object)
-  if object.getStateId() == 2 then
-    object.setState(1)
-  end
-  return container.hasMatchingTag(object)
-    or not container.hasAnyTag()
+    if object.getStateId() == 2 then
+        object.setState(1)
+    end
+    return container.hasMatchingTag(object)
 end
 
 ----------------------------------------------------
@@ -390,7 +350,7 @@ end
 function getOrderedPlayers()
     local seated_players = getSeatedPlayers()
 
-    local player_count = debug_player_count and debug_player_count or #seated_players
+    local player_count = #seated_players
 
     if (player_count > 4 or player_count < 2) then
         msg = "This game only supports 2-4 players"
@@ -441,6 +401,17 @@ function getOrderedPlayers()
     return ordered_players
 end
 
+function takeInitiative(color)
+    if color == "" then
+        return
+    end
+
+    local initiative_marker = getObjectFromGUID(initiative_GUID)
+    local player_board = getObjectFromGUID(player_pieces_GUIDs[color]["player_board"])
+    local initiative_pos = player_board.positionToWorld(initaitive_player_position)
+    initiative_marker.setPositionSmooth(initiative_pos)
+end
+
 function dealGuildCards(qty)
 
     local court_zone = getObjectFromGUID(court_deck_zone_GUID)
@@ -458,6 +429,45 @@ function dealGuildCards(qty)
         })
     end
 
+end
+
+-- params = {"psionics", "relics",  "weapons", "fuel", "materials"}
+function merchantSetup(params)
+
+    for _, resource in ipairs(params) do
+        local zone_pos
+
+        if (resource == "materials") then
+            local ambition_zone = getObjectFromGUID(
+                merchant_GUID["tycoon"])
+            zone_pos = ambition_zone.getPosition()
+            zone_pos.z = zone_pos.z - 0.5
+        elseif (resource == "fuel") then
+            local ambition_zone = getObjectFromGUID(
+                merchant_GUID["tycoon"])
+            zone_pos = ambition_zone.getPosition()
+            zone_pos.z = zone_pos.z + 0.8
+        elseif (resource == "weapons") then
+            local ambition_zone = getObjectFromGUID(
+                merchant_GUID["warlord"])
+            zone_pos = ambition_zone.getPosition()
+        elseif (resource == "relics") then
+            local ambition_zone = getObjectFromGUID(
+                merchant_GUID["keeper"])
+            zone_pos = ambition_zone.getPosition()
+        elseif (resource == "psionics") then
+            local ambition_zone = getObjectFromGUID(
+                merchant_GUID["empath"])
+            zone_pos = ambition_zone.getPosition()
+        end
+
+        local token = getObjectFromGUID(resources_GUID[resource])
+
+        token.takeObject({
+            position = {zone_pos.x, zone_pos.y, zone_pos.z}
+        })
+
+    end
 end
 
 ----------------------------------------------------
@@ -1315,9 +1325,9 @@ starting_pieces = {
 
 function onLoad()
     Counters.setup()
-    Supplies.addMenuToAllObjects()
     -- Assign all connected players to a color spot.
     -- for _, player in ipairs(Player.getPlayers()) do
     --     assignPlayerToAvailableColor(player)
     -- end
+
 end
