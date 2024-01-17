ed7d7ea6edcc9cd52cc3ebf6375446a99543e1e0
scyth02 <scyth02@gmail.com>
2024-01-17 17:53:28 -0500

    Added snap points for agents above the court cards.

--- tools/LayoutTools.lua
+++ tools/LayoutTools.lua
@@ -9,6 +9,7 @@ local player_boards = {
     ["Teal"]    = "ae512a"
 }
 
+-- City snaps on play board
 local cities_layout = {
     {-0.255,0.978,-0.828},
     {-0.628,0.978,-0.829},
@@ -17,19 +18,36 @@ local cities_layout = {
     {-0.876,0.978,-0.829}
 }
 
+-- Resource snaps on player boards
 local resource_layout = {
-{0.863,0.209,-0.741},
-{0.614,0.209,-0.742},
-{0.365,0.209,-0.742},
-{-0.381,0.209,-0.743},
-{0.116,0.209,-0.742},
-{-0.132,0.209,-0.742}
+    {0.863,0.209,-0.741},
+    {0.614,0.209,-0.742},
+    {0.365,0.209,-0.742},
+    {-0.381,0.209,-0.743},
+    {0.116,0.209,-0.742},
+    {-0.132,0.209,-0.742}
+}
+
+-- Agent snaps above court cards
+local court_agent_layout = {
+    {-0.75,0.825,-2.5}, 
+    {-0.45,0.825,-2.5},
+    {-0.15,0.825,-2.5},  
+    {0.15,0.825,-2.5}, 
+    {0.45,0.825,-2.5}, 
+    {0.75,0.825,-2.5}, 
+    {-0.75,0.825,-1.9}, 
+    {-0.45,0.825,-1.9}, 
+    {-0.15,0.825,-1.9},
+    {0.15,0.825,-1.9},
+    {0.45,0.825,-1.9},  
+    {0.75,0.825,-1.9}
 }
 
 function LayoutTools.repositionSet()
-    local board = getObjectFromGUID("ae512a")
+    local board = getObjectFromGUID("89ddf3")
     for ct,object in ipairs(getObjectsWithTag("pos_find")) do
-        local pos = resource_layout[ct]; pos = board.positionToWorld(pos)
+        local pos = court_agent_layout[ct]; pos = board.positionToWorld(pos)
         object.setPosition(pos)
     end
 end
