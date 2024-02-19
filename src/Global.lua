2c94e6b3898275abfebeb16c75a5dcdedecb0f44
scyth02 <scyth02@gmail.com>
2024-02-19 12:48:19 -0500

    Enabled debugging

--- src/Global.lua
+++ src/Global.lua
@@ -5,7 +5,7 @@ available_colors = {"White", "Yellow", "Red", "Teal"}
 ----------------------------------------------------
 -- [DEBUG] REMEMBER TO SET TO FALSE BEFORE RELEASE
 ----------------------------------------------------
-debug = false
+debug = true
 debug_player_count = 3
 ----------------------------------------------------
 
