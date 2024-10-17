cb85ae4e456c3d48bd4240ce6ffe08bce0c83c6d
Travis Ladner <travis@tladner.com>
2024-10-16 22:27:59 -0700

    allow lack of surpass to announce End Round

--- src/Control.lua
+++ src/Control.lua
@@ -124,21 +124,28 @@ function start_chapter()
     end
 
     ActionCards.deal_hand()
+
+    local initiative_player = Global.getVar("initiative_player")
+    print(initiative_player .. " will start the chapter", initiative_player)
+    Turns.turn_color = initiative_player
 end
 
 function end_round()
     ActionCards.clear_played()
     AmbitionMarkers.reset_zero_marker()
+    local initiative_player = Global.getVar("initiative_player")
 
     -- Auto Initiative, Find surpassing card
     if (Initiative.is_seized()) then
         Initiative.unseize()
-        Turns.turn_color = Initiative.player
+        Turns.turn_color = initiative_player
+        broadcastToAll(initiative_player .. " had seized initiative", initiative_player)
     else
         local surpassing = ActionCards.get_surpassing_card()
         if not surpassing then
-            Turns.turn_color = Initiative.player
-            return
+            Turns.turn_color = initiative_player
+            broadcastToAll("No surpass or seize, ".. initiative_player .. " keeps initiative.", initiative_player)
+
         else
             local all_players = Global.getVar("active_players")
             for _, p in ipairs(all_players) do
@@ -146,16 +153,17 @@ function end_round()
                    p.last_action_card.type == surpassing.type and
                    p.last_action_card.number == surpassing.number then
                     Initiative.unseize()
-                    Initiative.take(p.color)
+                    Initiative.take(p.color, true)
                     Turns.turn_color = p.color
                     p.last_action_card = nil
+                    broadcastToAll(p.color .. " has surpassed and takes initiative.", p.color)
                     break
                 end
             end
         end
     end
 
-    broadcastToAll("End Round\n")
+    broadcastToAll("End Round\n", Color.Purple)
 end
 
 function take_initiative(objectButtonClicked, playerColorClicked)
