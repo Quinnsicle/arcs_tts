0b211813653c3c58c3ee391c5ebae69afa93bed4
Travis Ladner <travis@tladner.com>
2024-11-26 12:50:19 -0800

    don't start timer automatically at chapter start

--- src/Control.lua
+++ src/Control.lua
@@ -128,7 +128,6 @@ function start_chapter()
     local initiative_player = Global.getVar("initiative_player")
     broadcastToAll(initiative_player .. " will start the chapter\n", initiative_player)
     Turns.turn_color = initiative_player
-    Global.call("startTimer")
 end
 
 function end_round()
